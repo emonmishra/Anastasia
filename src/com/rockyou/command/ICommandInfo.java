2263cbcc7d2bca5c5e43837cff04a50763523f2d
Prasanta Sahoo <Sahoo.Prasanta@servesharp.com>
2017-06-29 15:05:13 +0530

    Command specific

---
+++ src/com/rockyou/command/ICommandInfo.java
@@ -0,0 +1,11 @@
+package com.rockyou.command;
+
+import com.rockyou.storage.Key;
+import com.rockyou.storage.Value;
+
+public interface ICommandInfo {
+
+	public Key getKey() ;
+	public Value getValue();
+	public int expireTime();
+}
--- src/com/rockyou/server/TcpServer.java
+++ src/com/rockyou/server/TcpServer.java
@@ -2,8 +2,14 @@ package com.rockyou.server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 import com.rockyou.command.CommandManager;
 
@@ -11,6 +17,11 @@ public class TcpServer {
 
 	private CommandManager commandMgr;
 	private ServerSocketChannel serverSocketChannel;
+	private Selector selector;
+
+	public TcpServer() {
+		commandMgr = new CommandManager();
+	}
 
 	public boolean startServer(int port) {
 		return startServer(new InetSocketAddress(port));
@@ -19,27 +30,61 @@ public class TcpServer {
 	public boolean startServer(InetSocketAddress sockAddr) {
 		try {
 			serverSocketChannel = ServerSocketChannel.open();
+			serverSocketChannel.socket().bind(sockAddr);
+			serverSocketChannel.configureBlocking(false);
+			
+			selector = Selector.open();
+			serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT, null);
+
+		} catch (ClosedChannelException e1) {
+			return false;
 		} catch (IOException e) {
 			//Log error
 			return false;
 		}
 
-		try {
-			serverSocketChannel.socket().bind(sockAddr);
-		} catch (IOException e) {
-		}
-
 		while(true){
-		    try {
-				SocketChannel socketChannel = serverSocketChannel.accept();
-			} catch (IOException e) {
-			}
+			try {
+				selector.select();
 
-		    //do something with socketChannel...
+				Set<SelectionKey> keys = selector.selectedKeys();
+				Iterator<SelectionKey> keyIterator = keys.iterator();
+
+				while (keyIterator.hasNext()) {
+					SelectionKey myKey = keyIterator.next();
+	 
+					if (myKey.isAcceptable()) {
+						SocketChannel client = serverSocketChannel.accept();
+						client.configureBlocking(false);
+	 
+						client.register(selector, SelectionKey.OP_READ);
+					} else if (myKey.isReadable()) {
+						read(myKey);
+					}
+					keyIterator.remove();
+				}
+
+			} catch (IOException e1) {
+			}
 		}
 	}
 
+	private void read(SelectionKey key) throws IOException {
+		SocketChannel channel = (SocketChannel) key.channel();
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		int numRead = -1;
+		numRead = channel.read(buffer);
+
+		if (numRead == -1) {
+			channel.close();
+			key.cancel();
+			return;
+		}
+
+	}
+
 	public boolean stopServer() {
+		
 		return true;
 	}
 }
